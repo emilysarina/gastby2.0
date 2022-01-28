17f34389ccd4a7025fd99ad2562819b53b3f0a70
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-01-28 16:21:29 +0000

    added dropdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,16 @@ import { useState } from 'react';
 
 import { Link } from 'gatsby';
 
+import LondonData from './london.js';
+import NewYorkData from './newyork.js';
+import TokyoData from './tokyo.js';
+
 const IndexPage = () => {
+  const [value, setValue] = useState('');
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
 
   return (
     <main>
@@ -11,6 +20,21 @@ const IndexPage = () => {
       <Link to="/london">London</Link>
       <Link to="/tokyo">Tokyo</Link>
       <Link to="/newyork">New York</Link>
+      <select onChange={handleChange} name="cars" id="cars">
+        <option value="">Select City</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Tokyo">Tokyo</option>
+      </select>
+      {value === 'London' ? (
+        <LondonData />
+      ) : value === 'New York' ? (
+        <NewYorkData />
+      ) : value === 'Tokyo' ? (
+        <TokyoData />
+      ) : (
+        ''
+      )}
     </main>
   );
 };
